3c58515929192979643aa6193316b97633930796
Carlos Feria <carlosthe19916@gmail.com>
2016-03-07 18:20:53 -0500

    config skeleton

--- app/modules/venta/config/venta.client.routes.js
+++ app/modules/venta/config/venta.client.routes.js
@@ -24,6 +24,84 @@ angular.module('venta').config(['$stateProvider', '$urlRouterProvider',
         ncyBreadcrumb: {
           skip: true // Never display this state in breadcrumb.
         }
+      })
+
+      .state('venta.app.caja', {
+        url: '/caja',
+        template: '<div ui-view></div>',
+        abstract: true
+      })
+      .state('venta.app.operaciones', {
+        url: '/operaciones',
+        template: '<div ui-view></div>',
+        abstract: true
+      })
+
+      .state('venta.app.caja.abrir', {
+        url: '/abrir',
+        templateUrl: 'modules/venta/views/caja/caja.abrir.html',
+        controller: 'Venta.Caja.AbrirController',
+        resolve: {
+
+        },
+        ncyBreadcrumb: {
+          skip: true
+        }
+      })
+      .state('venta.app.caja.cerrar', {
+        url: '/cerrar',
+        templateUrl: 'modules/venta/views/caja/caja.cerrar.html',
+        controller: 'Venta.Caja.CerrarController',
+        resolve: {
+
+        },
+        ncyBreadcrumb: {
+          skip: true
+        }
+      })
+      .state('venta.app.caja.movimientos', {
+        url: '/movimientos',
+        template: '<div ui-view></div>',
+        ncyBreadcrumb: {
+          skip: true // Never display this state in breadcrumb.
+        }
+      })
+      .state('venta.app.caja.movimientos.buscar', {
+        url: '/buscar',
+        templateUrl: 'modules/venta/views/caja/caja.movimientos.buscar.html',
+        controller: 'Venta.Caja.Movimientos.BuscarController',
+        resolve: {
+
+        },
+        ncyBreadcrumb: {
+          label: 'Home'
+        }
+      })
+      .state('venta.app.caja.movimientos.editar', {
+        url: '/editar/:venta',
+        templateUrl: 'modules/venta/views/caja/caja.movimientos.editar.html',
+        resolve: {
+          /*tipoDocumento: function ($state, $stateParams, SGTipoDocumento) {
+            return SGTipoDocumento.$find($stateParams.documento);
+          }*/
+        },
+        controller: 'Venta.Caja.Movimientos.EditarController',
+        ncyBreadcrumb: {
+          label: 'Editar venta',
+          parent: 'venta.app.caja.movimiento.buscar'
+        }
+      })
+
+      .state('venta.app.operaciones.venta', {
+        url: '/venta',
+        templateUrl: 'modules/venta/views/venta/venta.crear.html',
+        controller: 'Venta.CrearController',
+        resolve: {
+
+        },
+        ncyBreadcrumb: {
+          skip: true
+        }
       });
   }
 ]);
---
+++ app/modules/venta/controllers/caja/caja.movimientos.buscar.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* jshint -W098 */
+angular.module('venta').controller('Venta.Caja.Movimientos.BuscarController',
+  function ($scope, $state) {
+
+
+    $scope.detalle = [
+      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
+      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
+      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
+      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
+      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } }
+    ];
+
+  }
+);
