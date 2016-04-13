63b97e5c192ed9ab54e79545269c04d75d132d93
carlosthe19916 <carlosthe19916@gmail.com>
2016-04-13 18:07:48 -0500

    add some features on venta

--- app/modules/venta/controllers/operaciones/venta/venta.busqueda.manual.controller.js
+++ app/modules/venta/controllers/operaciones/venta/venta.busqueda.manual.controller.js
@@ -4,14 +4,20 @@
 angular.module('venta').controller('Venta.Busqueda.BusquedaProducto.ManualController',
   function ($scope, $state) {
 
+    $scope.view = function () {
+        producto: undefined
+    };
 
-    $scope.detalle = [
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } }
-    ];
+    $scope.addProducto = function () {
+      if(!$scope.form.$invalid) {
+        var producto = angular.copy($scope.view.producto);
+        $scope.view.producto = undefined;
+        $scope.$parent.view.productos.push(producto);
+        $scope.form.$submitted = false;
+      } else {
+        $scope.form.$submitted = true;
+      }
+    };
 
   }
 );
--- app/modules/venta/controllers/operaciones/venta/venta.crear.controller.js
+++ app/modules/venta/controllers/operaciones/venta/venta.crear.controller.js
@@ -2,16 +2,46 @@
 
 /* jshint -W098 */
 angular.module('venta').controller('Venta.CrearController',
-  function ($scope, $state) {
+  function ($scope, $state, toastr, SCDialog, OSPersona) {
 
+    $scope.view = {
+      productos: []
+    };
 
-    $scope.detalle = [
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } },
-      { denominacion: 0.20, cantidad: 0, getSubtotal: function () { return 0; } }
-    ];
+    $scope.view.load = {
+      cliente: {}
+    };
+
+    $scope.buscarCliente = function ($event) {
+      if (!angular.isUndefined($event)) {
+        $event.preventDefault();
+      }
+
+      if(!$scope.view.load.cliente.numeroDocumento) {
+        $scope.view.load.cliente = {};
+        return;
+      }
+
+      //Warning! hard code set on criteria.
+      var criteria = {
+        tipoDocumento: 37,
+        numeroDocumento: $scope.view.load.cliente.numeroDocumento
+      };
+      OSPersona.$getAll(criteria).then(function (response) {
+        $scope.view.load.cliente = response[0];
+        if ($scope.view.load.cliente) {
+          toastr.info('Persona encontrada');
+        } else {
+          toastr.warning('Persona no encontrada');
+        }
+      });
+    };
+
+    $scope.devolverProducto = function (item, $index) {
+      SCDialog.confirm('Guardar', 'Estas seguro de devolver el producto?', function () {
+        $scope.view.productos.splice($index, 1);
+      });
+    };
 
   }
 );
