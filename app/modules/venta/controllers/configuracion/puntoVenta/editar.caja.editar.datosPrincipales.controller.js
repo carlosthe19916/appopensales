b1d7cb952afbb837c3aef3c2060fec9d7755f743
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2016-04-21 18:17:57 -0500

    Update editar.caja.editar.datosPrincipales.controller.js

--- app/modules/venta/controllers/configuracion/puntoVenta/editar.caja.editar.datosPrincipales.controller.js
+++ app/modules/venta/controllers/configuracion/puntoVenta/editar.caja.editar.datosPrincipales.controller.js
@@ -19,7 +19,7 @@ angular.module('venta').controller('Venta.Configuracion.PuntoVenta.Caja.Editar.D
         },
         function error(err) {
           $scope.working = false;
-          toastr.error(err.data.message);
+          toastr.error(err.data.errorMessage);
         }
       );
     };
