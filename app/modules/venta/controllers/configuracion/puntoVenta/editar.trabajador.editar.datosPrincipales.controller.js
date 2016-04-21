79c8c5d937f2a917859474190175dd00f4dc8f41
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2016-04-21 18:18:35 -0500

    Update editar.trabajador.editar.datosPrincipales.controller.js

--- app/modules/venta/controllers/configuracion/puntoVenta/editar.trabajador.editar.datosPrincipales.controller.js
+++ app/modules/venta/controllers/configuracion/puntoVenta/editar.trabajador.editar.datosPrincipales.controller.js
@@ -19,7 +19,7 @@ angular.module('venta').controller('Venta.Configuracion.PuntoVenta.Trabajador.Ed
         },
         function error(err) {
           $scope.working = false;
-          toastr.error(err.data.message);
+          toastr.error(err.data.errorMessage);
         }
       );
     };*/
